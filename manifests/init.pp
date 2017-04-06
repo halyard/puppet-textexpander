a102565446c1cf5e389e1f18a9a8dfc95906d0c2
Les Aker <me@lesaker.org>
2017-04-05 23:17:13 -0400

    fix linting

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,12 +8,12 @@ class textexpander (
   package { 'textexpander-halyard':
     provider => 'brewcask',
     require  => Homebrew::Tap['halyard/casks']
-  } ->
-  osx_login_item { 'TextExpander':
+  }
+  -> osx_login_item { 'TextExpander':
     hidden => true,
     path   => $install_path
-  } ~>
-  exec { 'launch textexpander':
+  }
+  ~> exec { 'launch textexpander':
     command     => "/usr/bin/open ${install_path}",
     refreshonly => true,
   }
